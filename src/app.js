33fd7f87504e573ac2a9abee6402caef74720a0d
Erin Gurley <erinmgurley@gmail.com>
2019-04-26 17:01:15 -0700

    changed classes to values

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import feedSushis from './feed-sushis.js';
 const sushiButtons = document.querySelectorAll('.sushi');
-const sushi = document.getElementById('sushi');
+console.log(sushiButtons);
+// const gimbap = document.getElementById('gimbap');
+// const roe = document.getElementById('roe');
+// const salshimi = document.getElementById('salshimi');
+// const salmon = document.getElementById('salmon');
+// const tuna = document.getElementById('tuna');
+const girl = document.getElementById('girl');
+
+
 
 for(let i = 0; i < sushiButtons.length; i++) {
-    const sushiButton = sushiButton [i];
+    const sushiButton = sushiButtons[i];
     
     sushiButton.addEventListener('click', () => {
-        feedSushis(sushiButton.value, sushi);
+        feedSushis(sushiButton.value, girl);
     });
+
 } 
--- test/tests.js
+++ test/tests.js
@@ -5,9 +5,6 @@ test('Test adding a sushi segment', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const sushiName = 'sushi';
-    //placeholder for the section that caterpillar lives in
-    const div = document.createElement('div');
-    const expected = '<span class="part sushi"></span>';
 
     //Act 
     // Call the function you're testing and set the result to a const
