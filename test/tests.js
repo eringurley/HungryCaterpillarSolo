5f8ed43628ab7c3dc28748e882ba696f1eb9a42a
Erin Gurley <erinmgurley@gmail.com>
2019-04-28 14:56:35 -0700

    and const div and const expected for passing travis test

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,8 @@ test('Test adding a sushi segment', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const sushiName = 'sushi';
+    const div = document.createElement('div');
+    const expected = '<span class="part sushi"></span>';
 
     //Act 
     // Call the function you're testing and set the result to a const
