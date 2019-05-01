52926466c95e0818ee9b03df1013b6c59650ad7b
Erin Gurley <erinmgurley@gmail.com>
2019-05-01 16:54:07 -0700

    fixed spacing in index and removed commented out code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 import feedSushis from './feed-sushis.js';
 const sushiButtons = document.querySelectorAll('.sushi');
-console.log(sushiButtons);
-// const gimbap = document.getElementById('gimbap');
-// const roe = document.getElementById('roe');
-// const salshimi = document.getElementById('salshimi');
-// const salmon = document.getElementById('salmon');
-// const tuna = document.getElementById('tuna');
 const girl = document.getElementById('girl');
-
-
 
 for(let i = 0; i < sushiButtons.length; i++) {
     const sushiButton = sushiButtons[i];
