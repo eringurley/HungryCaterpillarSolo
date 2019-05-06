036c49a2289dffcd956af4a4b8b1d61389e6d17a
Erin Gurley <erinmgurley@gmail.com>
2019-05-05 17:20:11 -0700

    deleted danceSushis

--- src/dance.js
+++
@@ -1,8 +0,0 @@
-function danceSushi(sushiName, parentElement) {
-    const span = document.createElement('span');
-    span.classList.add('part', sushiName);
-    
-    parentElement.appendChild(span);
-    return parentElement.innerHTML;
-}
-export default danceSushi;
